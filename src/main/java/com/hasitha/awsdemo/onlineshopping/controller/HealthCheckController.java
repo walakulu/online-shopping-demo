39c553817f68582f7cce3fc1b042681d3e500bf5
Hasitha Maduranga <maduranga.bbnt@gmail.com>
2025-02-23 16:03:30 +0100

    Add health check endpoint

---
+++ src/main/java/com/hasitha/awsdemo/onlineshopping/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.hasitha.awsdemo.onlineshopping.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
